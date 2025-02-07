ad35d592594717fb502702447cb420be6ee6b2c5
chasealexiei29 <chasealexei29@gmail.com>
2025-02-07 17:21:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ function openEnvelope() {
     document.querySelector('.flap').style.transform = "rotateX(180deg)";
     document.querySelector('.letter').style.opacity = "1";
     document.querySelector('.letter').style.transform = "translateY(0px)";
+    
+    // Hide the heart when the envelope is opened
+    document.querySelector('.heart').style.opacity = "0";
 }
